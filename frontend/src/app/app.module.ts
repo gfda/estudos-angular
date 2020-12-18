aab76ac1bceeb733a66ec7686b7f28043e647433
Gustavo Dias A <gfdiasa@gmail.com>
2020-12-17 21:15:32 -0300

    Create product view

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,13 +13,16 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
