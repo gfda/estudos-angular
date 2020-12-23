4bcc40ba2c22cc958d18157967b22dc1f08442a2
Gustavo Dias A <gfdiasa@gmail.com>
2020-12-22 22:05:11 -0300

    Create service add product

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
+import { ProductService } from '../product.service';
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
   }
+  
+  createProduct(): void {
+    this.productService.showMessage('Beer add with success!')
+  }
 
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
