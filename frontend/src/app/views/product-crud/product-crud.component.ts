8ac70c45d1bdd4d5775a1d919dbb90a897de1a14
Gustavo Dias A <gfdiasa@gmail.com>
2021-02-02 20:13:48 -0300

    Change views to use product service

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 @Component({
   selector: 'app-product-crud',
   templateUrl: './product-crud.component.html',
@@ -7,7 +8,13 @@ import { Router } from '@angular/router';
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private headerService: HeaderService) { 
+    headerService.headerData = {
+      title: 'Beer Registration',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
+  }
 
   ngOnInit(): void {
   }
