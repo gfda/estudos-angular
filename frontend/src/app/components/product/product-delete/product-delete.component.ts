ac0eb49d854d2a2661d3e86e6fc47ad4e1f7fbd9
Gustavo Dias A <gfdiasa@gmail.com>
2021-02-02 20:12:07 -0300

    Fix type mismatch

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -20,7 +20,7 @@ export class ProductDeleteComponent implements OnInit {
 
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id')
+    const id = this.route.snapshot.paramMap.get('id')
     this.productService.readById(id).subscribe(product => {
       this.product = product
     })
--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -18,7 +18,7 @@ export class ProductUpdateComponent implements OnInit {
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id')
+    const id = this.route.snapshot.paramMap.get('id')
     this.productService.readById(id).subscribe(product => {
       this.product = product
     })
