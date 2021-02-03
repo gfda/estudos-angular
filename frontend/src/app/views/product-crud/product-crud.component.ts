ee26f23b90f1dfb7685bca848bd36f3941136a35
Gustavo Dias A <gfdiasa@gmail.com>
2021-02-03 19:18:28 -0300

    Change icon

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -11,7 +11,7 @@ export class ProductCrudComponent implements OnInit {
   constructor(private router: Router, private headerService: HeaderService) { 
     headerService.headerData = {
       title: 'Beer Registration',
-      icon: 'storefront',
+      icon: 'sports_bar',
       routeUrl: '/products'
     }
   }
