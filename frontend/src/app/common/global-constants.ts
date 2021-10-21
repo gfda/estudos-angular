0e444ecc390a3ada3a6d2c320bfddc08d719cc89
Gustavo Dias A <gfdiasa@gmail.com>
2021-10-20 23:03:07 -0300

    Create constants file

---
+++ frontend/src/app/common/global-constants.ts
@@ -0,0 +1,8 @@
+export class GlobalConstants {
+  
+  public static readonly BASE_URL = "http://localhost:3001/products"
+  public static readonly ERROR_MSG = "An error has occurred!"
+  public static readonly MSG_DURATION = 3000
+  public static readonly POSITION_RIGHT = "right"
+  public static readonly POSITION_TOP = "top"
+}
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -3,45 +3,40 @@ import { MatSnackBar }from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
 import { Product } from './product.model';
 import { EMPTY, Observable } from 'rxjs';
-import {map, catchError} from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { GlobalConstants } from 'src/app/common/global-constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private readonly BASE_URL = "http://localhost:3001/products"
-  private readonly ERROR_MSG = "An error has occurred!"
-  private readonly MSG_DURATION = 3000
-  private readonly POSITION_RIGHT = "right"
-  private readonly POSITION_TOP = "top"
-
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
   
   showMessage(msg: string): void {
     this.snackBar.open(msg,'', {
-      duration: this.MSG_DURATION,
-      horizontalPosition: this.POSITION_RIGHT,
-      verticalPosition: this.POSITION_TOP
+      duration: GlobalConstants.MSG_DURATION,
+      horizontalPosition: GlobalConstants.POSITION_RIGHT,
+      verticalPosition: GlobalConstants.POSITION_TOP
     })
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.BASE_URL, product).pipe(
+    return this.http.post<Product>(GlobalConstants.BASE_URL, product).pipe(
       map((obj) => obj),
       catchError((e) => this.errorHandler(e))
     );
   }
   
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.BASE_URL).pipe(
+    return this.http.get<Product[]>(GlobalConstants.BASE_URL).pipe(
       map((obj) => obj),
       catchError((e) => this.errorHandler(e))
     );
   }
 
   readById(id: number): Observable<Product> {
-    const url = `${this.BASE_URL}/${id}`
+    const url = `${GlobalConstants.BASE_URL}/${id}`
     return this.http.get<Product>(url).pipe(
       map((obj) => obj),
       catchError((e) => this.errorHandler(e))
@@ -49,7 +44,7 @@ export class ProductService {
   }
   
   update(product: Product): Observable<Product> {
-    const url = `${this.BASE_URL}/${product.id}`
+    const url = `${GlobalConstants.BASE_URL}/${product.id}`
     return this.http.put<Product>(url, product).pipe(
       map((obj) => obj),
       catchError((e) => this.errorHandler(e))
@@ -57,7 +52,7 @@ export class ProductService {
   }
   
   delete(id: number): Observable<Product> {
-    const url = `${this.BASE_URL}/${id}`
+    const url = `${GlobalConstants.BASE_URL}/${id}`
     return this.http.delete<Product>(url).pipe(
       map((obj) => obj),
       catchError((e) => this.errorHandler(e))
@@ -65,7 +60,7 @@ export class ProductService {
   }
   
   errorHandler(e: any): Observable<any> {
-    this.showMessage(this.ERROR_MSG);
+    this.showMessage(GlobalConstants.ERROR_MSG);
     return EMPTY;
   }
 }
